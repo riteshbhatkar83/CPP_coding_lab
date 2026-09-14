153911f258a947050e16372a147d4283dd3a84ba
riteshbhatkar83 <riteshbhatkar644@gmail.com>
2026-09-14 14:59:45 +0530

    Added AccessSpecifier_Private.cpp - private access specifier demo

---
+++ AccessSpecifier_Private.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+#pragma pack(1)
+class Demo
+{
+    public:
+       int i;       // 3 chararestic 
+       char ch;
+
+    private:   
+       float f;
+};
+
+int main()
+{
+    Demo dobj;  
+     
+    dobj.i = 11;          
+    dobj.ch = 'A';
+    dobj.f = 3.14;  //error
+
+    cout<<dobj.i<<"\n";
+    cout<<dobj.ch<<"\n";
+    cout<<dobj.f<<"\n";
+    
+    return 0;
+}

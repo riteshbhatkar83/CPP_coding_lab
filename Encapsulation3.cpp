1aac55d8142299e90c649b4b868f006386b3764e
riteshbhatkar83 <riteshbhatkar644@gmail.com>
2026-09-14 14:59:27 +0530

    Added Encapsulation3.cpp - advanced encapsulation example

---
+++ Encapsulation3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+#pragma pack(1)
+class Demo
+{
+    int i;
+    char ch;
+    float f;
+};
+
+int main()
+{
+    Demo dobj;  
+     
+    dobj.i = 11;          //error
+    dobj.ch = 'A';
+    dobj.f = 3.14;
+    
+    return 0;
+}

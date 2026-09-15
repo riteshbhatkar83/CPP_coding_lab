19d88a468211c8f1c4aa6a3f7a459719a557aea9
riteshbhatkar83 <riteshbhatkar644@gmail.com>
2026-09-15 12:23:31 +0530

    Added POP.cpp

---
+++ POP.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int value1=0, value2=0,result =0;
+    cout<<"Enter first number : \n";
+    cin>>value1;
+    cout<<"Enter sec number : \n";
+    cin>>value2;
+
+    result = value1 +value2;
+
+    cout<<"Ans is : "<<result<<"\n";
+    return 0;
+
+}

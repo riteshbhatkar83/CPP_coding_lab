36cce933314dc4425213ab09bdda7891010ea351
riteshbhatkar83 <riteshbhatkar644@gmail.com>
2026-09-16 14:56:18 +0530

    Added POPXX.cpp

---
+++ POPXX.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+struct Arthematic
+{
+    int no1 ;
+    int no2;
+
+};
+int main()
+{
+    Arthematic aobj1;
+    Arthematic aobj2;
+
+    aobj1.no1 = 0;
+        aobj1.no2 = 0;
+
+        aobj2.no1 = 10;
+        aobj2.no2 = 11;
+
+    return 0;
+
+}

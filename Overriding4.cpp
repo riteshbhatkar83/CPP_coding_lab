84afd8cbabf1753d2f31ae6047c880eea344c0e3
riteshbhatkar83 <riteshbhatkar644@gmail.com>
2026-09-23 13:41:05 +0530

    Added Overriding4.cpp

---
+++ Overriding4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+// Base class
+class Base {
+public:
+virtual void show() {
+        cout << "Base class show function" << endl;
+    }
+};
+
+// Derived class
+class Derived : public Base {
+public:
+    void show() {
+        cout << "Derived class show function" << endl;
+    }
+};
+
+
+
+int main() {
+
+    Base *bp= new Derived();
+
+    bp->show();   // function call
+
+    
+
+    return 0;
+}
+
+/////Derived class show function
+
+
+//ue use here virtal keyword
